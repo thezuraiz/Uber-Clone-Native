b3dcd8fc222237b8efc684715be1d87c017cb187
The Zuraiz <thezuraiz@gmail.com>
2025-12-03 17:17:24 +0500

    Add Native wind

---
+++ app/+not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const NotFound = () => {
+  return (
+    <View>
+      <Text>NotFound</Text>
+    </View>
+  );
+};
+
+export default NotFound;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,17 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { StatusBar } from "expo-status-bar";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import "../global.css";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
+    <SafeAreaView>
       <StatusBar style="auto" />
-    </ThemeProvider>
+      <Text className="font-bold">Hioi</Text>
+    </SafeAreaView>
   );
 }
